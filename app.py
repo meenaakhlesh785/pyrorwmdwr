dce9b7f9b838803e30b65eedb067730925a49231
meenaakhlesh785 <119953479+meenaakhlesh785@users.noreply.github.com>
2022-12-08 00:12:36 +0530

    update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
  
 
 
-TOKEN = '2109918986:AAE8KJ6Zvj0mmJxayX-fusTFWZ2fzm56EHA'
+TOKEN = '5134846346:AAET1geWckwB_PDsIBBeAitTCWB3lU08YRw'
 API_ID = 1774230
 API_HASH = "b0829cf5b62052d6c8adee27b02f1f00"
 #bot = telebot.TeleBot(TOKEN)
@@ -22,8 +22,12 @@ API_HASH = "b0829cf5b62052d6c8adee27b02f1f00"
 server = Flask(__name__)
 
 
+@Client.on_message(filters.command(["admin"]))
+async def settings(bot,message):
+  await message.reply_text("akhil")
+
 #@bot.message_handler(commands=['start'])
-def#def start(message):
+#def#def start(message):
     #bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 @server.route('/' + TOKEN, methods=['POST'])
@@ -31,7 +35,16 @@ def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
+
 if __name__ == "__main__":
+  app = Client(
+    "LibraryBot",
+    bot_token=TOKEN,#Config.BOT_TOKEN,
+    api_id=API_ID,#Config.API_ID,
+    api_hash=API_HASH,#Config.API_HASH,
+    #plugins=plugins, 
+    )
+  app.run()
   #threading.Thread(target=runAutoList, name='run_server_time', daemon=True).start()
   #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
   #server.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
